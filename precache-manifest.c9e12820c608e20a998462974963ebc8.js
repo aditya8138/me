700bf9f424728002ceb33d9af9cbc24017581dc0
Aditya <aditya.singh@modopayments.com>
2020-04-16 19:14:41 -0500

    Updates

---
+++ precache-manifest.c9e12820c608e20a998462974963ebc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc8eef39d6e56977c867a9f0fa9a6ea",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "68fa69f592b0e6a4a73b",
+    "url": "/me/static/css/main.1b1f33f1.chunk.css"
+  },
+  {
+    "revision": "f959a579ce88cf55a1dd",
+    "url": "/me/static/js/2.634893ad.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/me/static/js/2.634893ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fa69f592b0e6a4a73b",
+    "url": "/me/static/js/main.2604b310.chunk.js"
+  },
+  {
+    "revision": "053647d2342baad513b8",
+    "url": "/me/static/js/runtime-main.9f39c5d9.js"
+  },
+  {
+    "revision": "f307b6bdd3f926068dcd63cbae4b0fa8",
+    "url": "/me/static/media/monkey.f307b6bd.png"
+  }
+]);
